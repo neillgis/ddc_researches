bf9c63d226c66c7f95b13ec5ba5c491d0842e965
praphann <nackr.aork@gmail.com>
2021-02-09 11:18:16 +0700

    Again

--- resources/views/frontend/research.blade.php
+++ resources/views/frontend/research.blade.php
@@ -75,11 +75,25 @@
     <!-- START SUMMARY Total Box -->
       <div class="row">
         <div class="col-md-4 mx-auto">
+          <div class="small-box bg-danger mx-auto">
+            <div class="inner">
+              <h3> {{ empty($Total_publish_pro)?'0': $Total_publish_pro }} โครงการ</h3>
+              <br>
+              <p> โครงการวิจัยที่ตีพิมพ์ทั้งหมด </p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-dice-d20"></i>
+            </div>
+            <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
+          </div>
+        </div>
+
+        <div class="col-md-4 mx-auto">
           <div class="small-box bg-success mx-auto">
             <div class="inner">
               <h3> {{ empty($Total_research)?'0': $Total_research }} โครงการ</h3>
               <br>
-              <p> โครงการวิจัยทั้งหมด </p>
+              <p> โครงการวิจัยที่ตรวจสอบแล้ว </p>
             </div>
             <div class="icon">
               <i class="fas fa-microscope"></i>
@@ -102,19 +116,6 @@
           </div>
         </div>
 
-        <div class="col-md-4 mx-auto">
-          <div class="small-box bg-danger mx-auto">
-            <div class="inner">
-              <h3> {{ empty($Total_publish_pro)?'0': $Total_publish_pro }} โครงการ</h3>
-              <br>
-              <p> โครงการวิจัยที่ตีพิมพ์ทั้งหมด </p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-dice-d20"></i>
-            </div>
-            <!-- <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a> -->
-          </div>
-        </div>
       </div>
       <br>
     <!-- END SUMMARY Total Box -->
