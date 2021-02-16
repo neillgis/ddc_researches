9d6886f2016b530c3d1eb5ceb89679bf5d99d7d5
praphann <nackr.aork@gmail.com>
2021-02-16 19:21:10 +0700

    Update js.blade.php

--- resources/views/layout/js.blade.php
+++ resources/views/layout/js.blade.php
@@ -49,7 +49,7 @@
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
+<!-- <script src="{{-- asset('dist/js/pages/dashboard.js') --}}"></script> -->
 
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('dist/js/demo.js') }}"></script>
