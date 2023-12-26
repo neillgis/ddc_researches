6a0849579d41b40f0f4cd3c16d8d176427ae4ad3
sunddtmod <71490866+sunddtmod@users.noreply.github.com>
2023-12-26 09:24:17 +0700

    https

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,9 +1,11 @@
 
 <?php
-if($_SERVER["HTTPS"] != "on")
+$is_https=false;
+if (isset($_SERVER['HTTPS'])) $is_https=$_SERVER['HTTPS'];
+if ($is_https !== "on")
 {
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
+    header("Location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    exit(1);
 }
 ?>
 <!DOCTYPE html>
