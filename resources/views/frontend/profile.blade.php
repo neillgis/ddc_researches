92f9c3756e1513bf6706053f862705abc85a6966
buaykung <yutthana_chjabok@kkumail.com>
2025-03-24 15:07:59 +0700

    Update ProfilePage
    - Edit wrong label

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -252,7 +252,7 @@
             <div class="row">
                 <div class="col-md-6">
                     <label for="exampleInput1"> รหัสนักวิจัยจาก NRIIS </label>
-                    <span></br>ลิงค์ไปยังเว็บไซต์ <a href="https://nriis.go.th/Login.aspx">NRIIS</a></span>
+                    <span></br>ลิงก์ไปยังเว็บไซต์ <a href="https://nriis.go.th/Login.aspx">NRIIS</a></span>
                     @foreach($data as $value)
                         @if($value->orcid_id != "")
                           <div class="border p-2" style="background-color: #e9ecef;opacity: 1; font-size: 16px;">
@@ -268,7 +268,7 @@
 
                 <div class="col-md-6">
                     <label for="exampleInput1"> เลขประจำตัวนักวิจัยจาก ORCID ID </label>
-                    <span></br>ลิงค์ไปยังเว็บไซต์ <a href="https://orcid.org/signin">ORCID ID</a></span>
+                    <span></br>ลิงก์ไปยังเว็บไซต์ <a href="https://orcid.org/signin">ORCID ID</a></span>
                     @foreach($data as $value)
                         @if($value->orcid_id != "")
                           <div class="border p-2" style="background-color: #e9ecef;opacity: 1; font-size: 16px;">
