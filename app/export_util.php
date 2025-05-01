f8d7df7f748617ed5dd9d0106fbeee4e1a1ff691
buaykung <yutthana_chjabok@kkumail.com>
2025-05-01 14:45:22 +0700

    [Update] ExportUtil
    -add query column
    -add prepdata function
    -add header

--- app/export_util.php
+++ app/export_util.php
@@ -17,27 +17,59 @@ class export_util implements FromCollection, WithHeadings
     public function collection()
     {
         // return research::all();
+        $queryUtilExport = DB::table('db_research_project')
+                        ->rightjoin('db_utilization', 'db_research_project.id', '=', 'db_utilization.pro_id')
+                        ->leftjoin('users', 'db_research_project.users_id', '=', 'users.idCard')
 
-        return DB::table('db_research_project')
-                ->rightjoin('db_utilization', 'db_research_project.id', '=', 'db_utilization.pro_id')
-                ->leftjoin('users', 'db_research_project.users_id', '=', 'users.idCard')
+                        ->leftjoin('ref_util_status', 'db_utilization.status', '=', 'ref_util_status.id')
+                        ->leftjoin('ref_verified', 'db_utilization.verified', '=', 'ref_verified.id')
+                        ->select('db_utilization.pro_id',
+                                'db_utilization.users_id',
+                                'db_research_project.users_name',
+                                'users.deptName',
+                                'db_research_project.pro_name_en',
+                                'db_research_project.pro_name_th',
+                                'db_research_project.pro_end_date',
+                                'util_year',
+                                'db_research_project.pro_position',
+                                'util_type',
+                                'util_descrip',
+                                'ref_util_status.util_status',
+                                'ref_verified.verify_name',
+                                'db_utilization.created_at'
+                                )
+                        ->whereNull('db_utilization.deleted_at')
+                        ->orderBy('db_utilization.id', 'DESC')
+                        ->get();
 
-                ->leftjoin('ref_util_status', 'db_utilization.status', '=', 'ref_util_status.id')
-                ->leftjoin('ref_verified', 'db_utilization.verified', '=', 'ref_verified.id')
-                ->select('db_utilization.pro_id',
-                         'db_utilization.users_id',
-                         'db_research_project.users_name',
-                         'users.deptName',
-                         'util_type',
-                         'util_descrip',
-                         'ref_util_status.util_status',
-                         'ref_verified.verify_name',
-                         'db_utilization.created_at'
-                         )
-                ->whereNull('db_utilization.deleted_at')
-                ->orderBy('db_utilization.id', 'DESC')
-                ->get();
+        $positionMap = [
+            1 => 'ผู้วิจัยหลัก',
+            2 => 'ผู้วิจัยหลัก-ร่วม',
+            3 => 'ผู้วิจัยร่วม',
+            4 => 'ผู้ช่วยวิจัย',
+            5 => 'ที่ปรึกษาโครงการ',
+        ];
 
+        $prepUtilTable = $queryUtilExport->map(function ($item) use ($positionMap){
+            return[
+                "pro_id" => $item -> pro_id,
+                "users_id" => $item -> users_id,
+                "users_name" => $item -> users_name,
+                "deptName" => $item -> deptName,
+                "pro_name_en" => $item -> pro_name_en,
+                "pro_name_th" => $item -> pro_name_th,
+                "pro_end_date" => $item -> pro_end_date,
+                "util_year" => $item -> util_year,
+                "pro_position" => $positionMap[$item->pro_position] ?? 'ไม่ทราบตำแหน่ง',
+                "util_type" => $item -> util_type,
+                "util_descrip" => $item -> util_descrip,
+                "util_status" => $item -> util_status,
+                "verify_name" => $item -> verify_name,
+                "created_at" => $item -> created_at,
+            ];
+        });
+
+        return $prepUtilTable;
     }
 
 
@@ -48,6 +80,11 @@ class export_util implements FromCollection, WithHeadings
             'เลขบัตรปชช.',
             'ชื่อ-สกุล',
             'หน่วยงาน',
+            'ชื่อโครงการ (ENG)',
+            'ชื่อโครงการ (TH)',
+            'ปีที่เสร็จสิ้น',
+            'ปีที่นำไปใช้ประโยชน์',
+            'ตำแหน่งในโครงการวิจัย',
             'ประเภท',
             'คำอธิบาย',
             'สถานะของการนำไปใช้ประโยชน์',
